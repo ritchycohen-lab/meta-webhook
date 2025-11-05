ea92acb1e64b1e3dac85cab5bd89ef066f19ceac
ritchycohen-lab <ritchy.cohen@gmail.com>
2025-11-05 21:18:49 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const VERIFY_TOKEN = 'ulpancoach2025';
+
+app.get('/', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token === VERIFY_TOKEN) {
+    console.log('✅ WEBHOOK VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post('/', (req, res) => {
+  console.log('📩 New event received:', JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(⁠ 🚀 Server running on port ${PORT} ⁠));
