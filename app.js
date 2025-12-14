e62bd48bf73ffdb7068b5e0c02b25a53e304eee4
ritchycohen-lab <ritchy.cohen@gmail.com>
2025-12-14 10:29:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -150,10 +150,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log("🚀 Server running on port " + PORT)
 );
-
-// ------------------------------------------------------
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log("🚀 Server running on port " + PORT)
-);
-
